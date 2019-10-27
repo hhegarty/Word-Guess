241f2164788acc475e5ef2be49ed1567e03f7d1d
honora hegarty <honora.hegarty@verizon.net>
2019-10-26 20:48:27 -0400

    making if else statements

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,9 +1,24 @@
 var Wins = 0;
 var Losses = 0;
-var GuessesLeft = 9;
-var Guessessofar = 0;
-var Computerchoice = 0;
+var GuessesLeft;
+var Guessessofar;
+var Computerchoice;
 var Userguess = 0;
-    if (Computerchoice === Userguess){
+var Letters = ['a', 'b','c','d','e', 'f','g','h','i', 'j','k','l','m', 'n','o','p','q', 'r','s','t','u', 'v','w','x','y','z']
 
+reset();
+display();
+
+document.onkeyup = function(event){
+    var guess = event.key;
+        if (guess === Computerchoice){
+            win();
         }
+        else if (GuessesLeft - 1 === 0){
+            Lose();
+        }
+        else {
+            fail(guess);
+        }
+        display();
+}
